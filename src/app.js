2e82d18d030b2e4b024d1081f29cfdd5be781b3f
Neampi <piero.neam@gmail.com>
2024-11-12 17:51:40 -0500

    Se realizo un servidor api express con MongoDB

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { config } = require('dotenv')
+config()
+
+const bookRoutes = require('./routes/book.routes')
+
+
+//Usamos express para los middlewares
+const app = express()
+app.use(bodyParser.json()) // Parsear Bodys
+
+
+//Aca conectaremos la base de datos
+mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DB_NAME})
+const db= mongoose.connection;
+
+app.use('/books', bookRoutes)
+
+
+const port = process.env.PORT  || 3000
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto ${port}`)
+})
---
+++ src/routes/book.routes.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book.model')
+
+
+//MIDDLEWARE
+const getBook = async(req,res,next) => {
+    let book;
+    const { id } = req.params;
+
+    if(!id.match(/^[0-9a-fA-F]{24}$/)){
+        return res.status(404).json({
+            message: 'El id del libro no es valido'
+        })
+    }
+
+    try{
+        book = await Book.findById(id);
+        if(!book){
+            return res.status(404).json(
+                {
+                    message: 'El libro no fue encontrado'
+                }
+            )
+        }
+    }catch(error){
+        return res.status(500).json({
+            message: error.menssage
+        })
+    }
+
+    res.book = book;
+    next()
+}
+
+router.get('/', async (req,res) => {
+    try{
+        const books = await Book.find()
+
+        console.log('GET ALL', books)
+        if(books.length === 0){
+            res.status(204).json([])
+        }
+        res.json(books)
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+//CREAR NUEVO LIBRO [POST]
+router.post('/', async(req,res) => {
+    const {title, author, genre, publication_date} = req?.body
+
+    if(!title || !author || !genre || !publication_date){
+        return res.status(400).json({
+            message: 'Los campos titulo, autor ,genero y fdecha son obligatorios'
+        })
+    }
+
+    const book = new Book({
+        title,
+        author,
+        genre,
+        publication_date
+    })
+
+    try{
+        const newBook = await book.save()
+        console.log(newBook)
+        res.status(201).json(newBook)
+    }catch(error){
+        res.status(400).json({
+            message: error.menssage
+        })
+    }
+})
+
+router.get('/:id', getBook, async(req, res) => {
+    res.json(res.book)
+})
+
+router.put('/:id', getBook, async(req, res) => {
+    try{
+        const book = res.book
+        book.title = req.body.title || book.title;
+        book.author = req.body.author || book.author;
+        book.genre = req.body.genre || book.genre;
+        book.publication_date = req.body.publication_date || book.publication_date;
+
+        const updatedBook = await book.save()
+        res.json(updatedBook)
+    }catch(error){
+        res.status(400).json({ message: error.message})
+    }
+})
+
+router.patch('/:id', getBook, async(req, res) => {
+
+    if(!req.body.title && !req.body.author && !req.body.genre && !req.body.publication_date){
+        res.status(400).json({ message: 'Al menos uno de estos campos deben ser enviados: Titulo, Autor, Genero, Fecha'})
+    }
+
+    try{
+        const book = res.book
+        book.title = req.body.title || book.title;
+        book.author = req.body.author || book.author;
+        book.genre = req.body.genre || book.genre;
+        book.publication_date = req.body.publication_date || book.publication_date;
+
+        const updatedBook = await book.save()
+        res.json(updatedBook)
+    }catch(error){
+        res.status(400).json({ message: error.message})
+    }
+})
+
+router.delete('/:id', getBook, async(req, res) => {
+    try{
+        const book = res.book;
+        await book.deleteOne({
+            _id: book._id
+        })
+        res.json({
+            message: `El libro ${book.title} fue eliminado correctamente`
+        })
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+module.exports = router
